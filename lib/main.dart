59b5cd4c8d645f08f007cb4aad0e97b0342aa297
poteto <naonyan1135@gmail.com>
2024-12-02 21:21:24 +0900

    一から不要な機能を削除していってる

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,16 @@
-import 'package:earthquake_net/settings_page.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'models.dart';
-import 'package:intl/intl.dart';
+import "package:intl/intl.dart";
 import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:logger/logger.dart';
-import 'package:workmanager/workmanager.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-// 通知プラグインのインスタンスを作成
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-// 通知の初期化設定
-void initializeNotifications() async {
-  const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('@mipmap/ic_launcher');
-
-  const InitializationSettings initializationSettings = InitializationSettings(
-    android: initializationSettingsAndroid,
-  );
-
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-}
-
-// 通知を表示する関数
-Future<void> showNotification() async {
-  const AndroidNotificationDetails androidPlatformChannelSpecifics =
-      AndroidNotificationDetails(
-    'earthquake_update_channel', // チャネルID
-    'Earthquake Updates', // チャネル名
-    channelDescription:
-        'Notification channel for earthquake updates', // チャネルの説明
-    importance: Importance.max,
-    priority: Priority.high,
-    ticker: 'ticker',
-  );
-
-  const NotificationDetails platformChannelSpecifics =
-      NotificationDetails(android: androidPlatformChannelSpecifics);
-
-  await flutterLocalNotificationsPlugin.show(
-    0, // 通知ID
-    '更新情報', // 通知タイトル
-    '地震情報が更新されました。', // 通知内容
-    platformChannelSpecifics,
-  );
-}
-
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) async {
-    if (task == 'fetchEarthquakeData') {
-      // 地震データをフェッチするロジックをここに追加
-      List<Earthquake> isUpdated = await fetchEarthquakes();
-      if (isUpdated.isNotEmpty) {
-        // データが更新された場合は通知を表示
-        await showNotification();
-      }
-    }
-    return Future.value(true);
-  });
-}
+import "package:dio/dio.dart";
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
-  initializeNotifications(); // 通知の初期化
-  Workmanager().initialize(
-    callbackDispatcher, // 上で定義したコールバック関数
-    isInDebugMode: false, // デバッグモードをオフにする
-  );
-  Workmanager().registerPeriodicTask(
-    '1',
-    'fetchEarthquakeData',
-    frequency: const Duration(minutes: 15), // 15分ごとに実行
-    constraints: Constraints(
-      networkType: NetworkType.connected, // インターネット接続が必要
-    ),
-  );
   runApp(const MyApp());
 }
-
-// 以下、MyAppクラスとその他のウィジェット、関数などのコード...
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -108,164 +36,230 @@ class MyApp extends StatelessWidget {
           bodyMedium: TextStyle(color: Colors.white),
         ),
       ),
-      home: const _EarthquakePage(),
-    );
-  }
-}
-
-Future<List<Earthquake>> fetchEarthquakes() async {
-  try {
-    final response = await http
-        .get(Uri.parse('https://www.jma.go.jp/bosai/quake/data/list.json'));
-    if (response.statusCode == 200) {
-      List<dynamic> jsonResponse = jsonDecode(response.body);
-      return jsonResponse.map((item) => Earthquake.fromJson(item)).toList();
-    } else {
-      throw Exception(
-          'Failed to load earthquake data with status code: ${response.statusCode}');
-    }
-  } catch (e) {
-    // ここでエラーを処理する
-    // ignore: avoid_print
-    print('An error occurred while fetching earthquake data: $e');
-    throw Exception('Failed to fetch earthquake data');
-  }
-}
-
-class _EarthquakePage extends StatefulWidget {
-  const _EarthquakePage();
-
-  @override
-  _EarthquakePageState createState() => _EarthquakePageState();
-}
-
-class _EarthquakePageState extends State<_EarthquakePage> {
-  List<Earthquake> earthquakes = [];
-  final Logger logger = Logger();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadInitialData();
-  }
-
-  Future<void> _loadInitialData() async {
-    earthquakes = await fetchEarthquakes();
-    setState(() {});
-  }
-
-  Future<void> refreshEarthquakes() async {
-    List<Earthquake> newEarthquakes = await fetchEarthquakes();
-    setState(() {
-      earthquakes.insertAll(
-          0, newEarthquakes); // Add new data at the top of the list
-      // Assuming you want to keep a maximum of 50 items in the list
-      if (earthquakes.length > 50) {
-        earthquakes = earthquakes.sublist(0, 50);
-      }
-    });
-  }
-
-  String formatDateTime(String? dateStr) {
-    if (dateStr == null || dateStr.isEmpty) {
-      return '';
-    }
-    try {
-      var inputFormat = DateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-      var outputFormat = DateFormat("yyyy年MM月dd日HH時mm分");
-      var parsedDate = inputFormat.parse(dateStr);
-      return outputFormat.format(parsedDate);
-    } catch (e) {
-      logger.e('Error parsing date: $e');
-      return dateStr; // パース失敗時は元の文字列を返す
-    }
-  }
-
-  Future<void> openUrl(Uri url) async {
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      logger.e('Could not launch $url');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('地震情報'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => const SettingsPage(),
-     ));
-          },
-        ),
-      ],
-    ),
-    
-      body: RefreshIndicator(
-        onRefresh: refreshEarthquakes,
-        child: ListView.builder(
-          itemCount: earthquakes.length,
-          itemBuilder: (context, index) {
-            return Card(
-              margin: const EdgeInsets.all(8),
-              child: ListTile(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SelectableText('情報名: ${earthquakes[index].ttl}', // ttlを表示
-                        style: const TextStyle(fontSize: 18)),
-                    SelectableText(
-                        '日時: ${formatDateTime(earthquakes[index].at)}',
-                        style: const TextStyle(fontSize: 18)),
-                    RichText(
-                      text: TextSpan(
-                        style: DefaultTextStyle.of(context)
-                            .style
-                            .copyWith(fontSize: 18),
-                        children: <TextSpan>[
-                          const TextSpan(text: '震央地名: '),
-                          TextSpan(
-                            text: earthquakes[index].anm,
-                            style: const TextStyle(color: Colors.lightBlue),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                final query =
-                                    Uri.encodeComponent(earthquakes[index].anm);
-                                final googleMapsUrl =
-                                    "https://www.google.com/maps/search/?api=1&query=$query";
-                                launchUrl(Uri.parse(googleMapsUrl));
-                              },
-                          ),
-                        ],
-                      ),
-                    ),
-                    SelectableText('マグニチュード: ${earthquakes[index].mag}',
-                        style: const TextStyle(fontSize: 18)),
-                    SelectableText('最大震度: ${earthquakes[index].maxi}',
-                        style: const TextStyle(fontSize: 18)),
-                  ],
-                ),
-                trailing: IconButton(
-                  icon: const Icon(Icons.share),
-                  onPressed: () {
-                    final earthquakeInfo = '情報名: ${earthquakes[index].ttl}\n'
-                        '日時: ${formatDateTime(earthquakes[index].at)}\n'
-                        '震央地名: ${earthquakes[index].anm}\n'
-                        'マグニチュード: ${earthquakes[index].mag}\n'
-                        'https://地震.net'; // ここにURLを追加
-                    Share.share(earthquakeInfo);
-                  },
-                ),
-              ),
-            );
-          },
-        ),
+      // home:  _EarthquakePage(),
+      home: SafeArea(
+        child: _EarthQuake(),
       ),
     );
   }
 }
+
+class _EarthQuake extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _earthquakes =
+        useState<List<Earthquake>>([]); //* List型のearthquakesを作ってる
+    useEffect(() {
+      //* useEffectはasyncを使えないから.thenを使う
+      //* 初回のみfetchEarthquakesを呼び出して_earthquakesに値を入れる
+      fetchEarthquakes().then((earthquakes) {
+        _earthquakes.value = earthquakes;
+      });
+      return null; // Dispose function is not needed here
+    }, []);
+
+    if (_earthquakes.value.isEmpty) {
+      return const Center(
+        child: CircularProgressIndicator(), // ローディング表示
+      );
+    }
+
+    return Scaffold(
+        // body: CustomScrollView(
+        //   slivers: <Widget>[
+        //     SliverList(
+        //       delegate: SliverChildBuilderDelegate(
+        //         (BuildContext context, int index) {
+        //           return Container(
+        //             margin: const EdgeInsets.all(8),
+        //             child: Card(
+        //               child: Column(
+        //                 children: [
+        //                   Text('情報名: ${_earthquakes.value[index].ttl}'),
+        //                   Text('日時: ${_earthquakes.value[index].anm}'),
+        //                 ],
+        //               ),
+        //             ),
+        //           );
+        //         },
+        //         childCount: _earthquakes.value.length,
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        body: Center(
+      child: ListView.builder(
+          itemCount: 20,
+          itemExtent: 100.0, // 各アイテムの高さを指定
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+                margin: const EdgeInsets.all(8),
+                child: Card(
+                    child: Column(
+                  children: [
+                    SelectableText('情報名: ${_earthquakes.value[index].ttl}'),
+                    SelectableText('震源地名: ${_earthquakes.value[index].anm}'),
+                  ],
+                )));
+          }),
+    ));
+  }
+}
+
+Future<List<Earthquake>> fetchEarthquakes() async {
+  final dio = Dio();
+  final response =
+      await dio.get("https://www.jma.go.jp/bosai/quake/data/list.json");
+  //* jsonにしてる
+  if (response.statusCode == 200) {
+    List<dynamic> json = response.data; //* ここでlistにしてる
+    // print(json);
+    final earthquakes = json.map((item) => Earthquake.fromJson(
+        item)); //* 一個一個のitemをEarthquake.fromJsonでEarthquakeに変換してる
+    return earthquakes.toList();
+    //* ここでリストにしてる こうしないとあとでListViewの時にエラーが出る
+    //* List型ってのはjsonみたいな感じで[{},{}]のようになってる
+  } else {
+    throw Exception(
+        'Failed to load earthquake data with status code: ${response.statusCode}');
+  }
+}
+
+// Future<List<Earthquake>> fetchEarthquakes() async {
+//   try {
+//     final dio = Dio();
+//     final response =
+//         await dio.get('https://www.jma.go.jp/bosai/quake/data/list.json');
+//     if (response.statusCode == 200) {
+//       List<dynamic> jsonResponse = jsonDecode(response.data); // 修正箇所
+//       return jsonResponse.map((item) => Earthquake.fromJson(item)).toList();
+//     } else {
+//       throw Exception(
+//           'Failed to load earthquake data with status code: ${response.statusCode}');
+//     }
+//   } catch (e) {
+//     print('An error occurred while fetching earthquake data: $e');
+//     throw Exception('Failed to fetch earthquake data');
+//   }
+// }
+
+// class _EarthquakePage extends StatefulWidget {
+//   const _EarthquakePage();
+
+//   @override
+//   _EarthquakePageState createState() => _EarthquakePageState();
+// }
+
+// class _EarthquakePageState extends State<_EarthquakePage> {
+//   List<Earthquake> earthquakes = [];
+//   @override
+//   void initState() {
+//     super.initState();
+//     _loadInitialData();
+//   }
+
+//   Future<void> _loadInitialData() async {
+//     earthquakes = await fetchEarthquakes();
+//     setState(() {
+//       earthquakes = earthquakes;
+//     });
+//   }
+
+//   Future<void> refreshEarthquakes() async {
+//     List<Earthquake> newEarthquakes = await fetchEarthquakes();
+//     setState(() {
+//       earthquakes.insertAll(
+//           0, newEarthquakes); // Add new data at the top of the list
+//       // Assuming you want to keep a maximum of 50 items in the list
+//       if (earthquakes.length > 50) {
+//         earthquakes = earthquakes.sublist(0, 50);
+//       }
+//     });
+//   }
+
+//   String formatDateTime(String? dateStr) {
+//     if (dateStr == null || dateStr.isEmpty) {
+//       return '';
+//     }
+//     try {
+//       var inputFormat = DateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+//       var outputFormat = DateFormat("yyyy年MM月dd日HH時mm分");
+//       var parsedDate = inputFormat.parse(dateStr);
+//       return outputFormat.format(parsedDate);
+//     } catch (e) {
+//       return dateStr; // パース失敗時は元の文字列を返す
+//     }
+//   }
+
+//   Future<void> openUrl(Uri url) async {
+//     if (await canLaunchUrl(url)) {
+//       await launchUrl(url);
+//     } else {}
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('地震情報')),
+//       body: RefreshIndicator(
+//         onRefresh: refreshEarthquakes,
+//         child: ListView.builder(
+//           itemCount: earthquakes.length,
+//           itemBuilder: (context, index) {
+//             return Card(
+//               margin: const EdgeInsets.all(8),
+//               child: ListTile(
+//                 title: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     SelectableText('情報名: ${earthquakes[index].ttl}', // ttlを表示
+//                         style: const TextStyle(fontSize: 18)),
+//                     SelectableText(
+//                         '日時: ${formatDateTime(earthquakes[index].at)}',
+//                         style: const TextStyle(fontSize: 18)),
+//                     RichText(
+//                       text: TextSpan(
+//                         style: DefaultTextStyle.of(context)
+//                             .style
+//                             .copyWith(fontSize: 18),
+//                         children: <TextSpan>[
+//                           const TextSpan(text: '震央地名: '),
+//                           TextSpan(
+//                             text: earthquakes[index].anm,
+//                             style: const TextStyle(color: Colors.lightBlue),
+//                             recognizer: TapGestureRecognizer()
+//                               ..onTap = () {
+//                                 final query =
+//                                     Uri.encodeComponent(earthquakes[index].anm);
+//                                 final googleMapsUrl =
+//                                     "https://www.google.com/maps/search/?api=1&query=$query";
+//                                 launchUrl(Uri.parse(googleMapsUrl));
+//                               },
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                     SelectableText('マグニチュード: ${earthquakes[index].mag}',
+//                         style: const TextStyle(fontSize: 18)),
+//                     SelectableText('最大震度: ${earthquakes[index].maxi}',
+//                         style: const TextStyle(fontSize: 18)),
+//                   ],
+//                 ),
+//                 trailing: IconButton(
+//                   icon: const Icon(Icons.share),
+//                   onPressed: () {
+//                     final earthquakeInfo = '情報名: ${earthquakes[index].ttl}\n'
+//                         '日時: ${formatDateTime(earthquakes[index].at)}\n'
+//                         '震央地名: ${earthquakes[index].anm}\n'
+//                         'マグニチュード: ${earthquakes[index].mag}\n';
+//                     Share.share(earthquakeInfo);
+//                   },
+//                 ),
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/models.dart
+++ lib/models.dart
@@ -1,42 +1,15 @@
-// models.dart
-
 class Earthquake {
-  final int id;
   final String anm;
-  final String at;
-  final String mag;
-  final String maxi;
-  final String ttl; // ttlフィールドを追加
-
+  final String ttl;
   Earthquake({
-    this.id = 0,
     this.anm = '',
-    this.at = '',
-    this.mag = '',
-    this.maxi = '',
-    this.ttl = '', // 初期値を追加
+    this.ttl = "",
   });
 
   factory Earthquake.fromJson(Map<String, dynamic> json) {
     return Earthquake(
-      id: json['id'] ?? 0,
       anm: json['anm'] ?? '',
-      at: json['at'] ?? '',
-      mag: json['mag'] ?? '',
-      maxi: json['maxi'] ?? '',
-      ttl: json['ttl'] ?? '', // JSONから読み込む
+      ttl: json['ttl'] ?? '',
     );
   }
-
-  Map<String, dynamic> toMap() {
-    var map = <String, dynamic>{};
-    map["id"] = id;
-    map["anm"] = anm;
-    map["at"] = at;
-    map["mag"] = mag;
-    map["maxi"] = maxi;
-    map["ttl"] = ttl; // マップに追加
-    return map;
-  }
 }
-
